aaf513e4c0f9ab0b453cb241177eea89aa1bbf5e
yahyojondev <nabijonovdeveloper@gmail.com>
2024-04-22 09:03:01 +0500

    hardwork

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,54 @@
 import React from "react";
 import HomeHero from "../../components/homeHero/HomeHero";
+import rasm1 from "../../assets/images/absoluteimg.svg";
+import rasm2 from "../../assets/images/absoluteimg2.png";
+import rasm3 from "../../assets/images/absoluteimg3.png";
 
 function Home() {
   return (
     <div>
       <HomeHero />
+      <div className="container">
+        <div className="home__cardabsolute">
+          <div className="home__card">
+            <h4>FS - QUILTED MAXI CROSS BAG</h4>
+            <div>
+              <img src={rasm1} alt="" />
+              <div className="price__wrapper">
+                <span>$534,33</span>
+                <p>24% Off</p>
+              </div>
+            </div>
+            <h3>$299,43</h3>
+          </div>
+          <div className="home__card">
+            <div className="titles">
+              <h4>FS - QUILTED MAXI CROSS BAG</h4>
+              <h3>$299,43</h3>
+            </div>
+            <div className="img__text">
+              <img src={rasm2} alt="" />
+              <div>
+                <span>$534,33</span>
+                <p>24% Off</p>
+              </div>
+            </div>
+          </div>
+          <div className="home__card">
+            <div className="titles">
+              <h4>FS - QUILTED MAXI CROSS BAG</h4>
+              <div className="price__wrapper">
+                <span>$534,33</span>
+                <p>24% Off</p>
+              </div>
+            </div>
+            <div>
+              <img src={rasm3} alt="" />
+            </div>
+            <h3>$299,43</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
